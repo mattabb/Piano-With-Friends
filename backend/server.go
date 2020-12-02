b25c5956975abf5d9980cc716aa660771b30147b
MattAbb <abbenema@bc.edu>
2020-12-02 18:08:12 -0500

    fix

--- backend/server.go
+++ backend/server.go
@@ -4,7 +4,6 @@ import (
 	// Internal Dependencies
 	"log"
 	"net/http"
-	"os"
 
 	// External Dependencies
 	"github.com/gorilla/mux"
@@ -15,17 +14,13 @@ func main() {
 	log.Println("Starting the backend server at http://localhost:8000/")
 
 	router := mux.NewRouter()
+	port := "8000"
 
 	// Whitelists our front-end
 	router.Host("localhost:8080")
 
 	// Add routes
 	AddAppRoutes(router)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000"
-	}
 
 	// Port our back-end will connect to
 	log.Fatal(http.ListenAndServe(":"+port, router))
